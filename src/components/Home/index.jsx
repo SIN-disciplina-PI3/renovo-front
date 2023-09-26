03496e2daa9486d561cf22adac03895af6dd211c
devigor81 <igor.00000847958@unicap.br>
2023-09-25 23:07:43 -0300

    :tada: First Commit

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <>
+    <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default Header;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Home'
+
+const Home = () => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default Home;
