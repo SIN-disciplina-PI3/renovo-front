b8255673785aa8c7bd7b27ba634ce2196c2e3309
devigor81 <igor.00000847958@unicap.br>
2023-10-01 21:52:38 -0300

    feat: Component de ONGs criado

---
+++ src/components/CarouselOngs/index.jsx
@@ -0,0 +1,57 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+import { Box, Container, Spacer, Flex } from '@chakra-ui/react'
+import SectionOng from '../SectionOngs'
+
+function OngCarousel() {
+  const [ongs, setOngs] = useState([])
+
+  useEffect(() => {
+    const ong1 = {
+      nome: 'CasaRosa',
+      descricao: 'Abrigamos mulheres vítimas do câncer de mama.',
+      tags: [{nome: 'mulheres', variant: 'false'}, {nome: 'câncer de Mama', variant: 'true'}]
+    }
+    const ong2 = {
+      nome: 'Abrigo Pet',
+      descricao: 'Abrigamos bichinhos de rua para adoção.',
+      tags: [{nome: 'animais', variant: 'false'}, {nome: 'abrigo', variant: 'true'}]
+    }
+    const ong3 = {
+      nome: 'Hospitalhaços',
+      descricao: 'Levamos alegria a crianças de Hospitais Público.',
+      tags: [{nome: 'crianças', variant: 'false'}, {nome: 'hospital', variant: 'true'}]
+    }
+    const ong4 = {
+      nome: 'ÁguaSim',
+      descricao: 'Lutamos pelo direito de acesso à água para todos.',
+      tags: [{nome: 'acesso à água', variant: 'true'}]
+    }
+    const ong5 = {
+      nome: 'Arca',
+      descricao: 'Reinserção social de crianças e adolescentes',
+      tags: [{nome: 'crianças', variant: 'true'}, {nome: 'adolescentes', variant: 'true'}]
+    }
+
+    setOngs([
+      ong1,
+      ong2,
+      ong3,
+      ong4,
+      ong5
+    ])
+  }, [])
+
+  return (
+      <Flex flexWrap="nowrap" justifyContent="space-between">
+        {ongs.map((ong, index) => (
+          <Box key={index} margin={3}>
+            <SectionOng ongProp={ong} />
+          </Box>
+        ))}
+      </Flex>
+  )
+}
+
+export default OngCarousel
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,27 @@
-import SectionProduct from "../../components/SectionProduct";
-import Navbar from "../../components/Navbar"
+import Navbar from '../../components/Navbar'
 import React from 'react'
-import { Box} from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 import ProductCarousel from '../../components/Carousel'
+import OngCarousel from '../../components/CarouselOngs'
 
 const Home = () => {
   return (
     <>
-    <Navbar />
-    <Box marginLeft="20%" w="60%">
-      <ProductCarousel />
-    </Box>
+      <Navbar />
+
+      <Box marginLeft="20%" w="60%">
+        <Text fontSize="2xl" color="#17214D">
+          Novidades
+        </Text>
+        <ProductCarousel />
+      </Box>
+
+      <Box marginLeft="20%" w="60%">
+        <Text fontSize="2xl" color="#17214D">
+          ONGs
+        </Text>
+        <OngCarousel />
+      </Box>
     </>
   )
 }
