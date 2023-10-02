0a298d62cc975a0bff064c58e10f3d9e5bb64a1c
devigor81 <igor.00000847958@unicap.br>
2023-10-01 23:39:09 -0300

    feat: Header criado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,12 +4,16 @@ import { Box, Text } from '@chakra-ui/react'
 import ProductCarousel from '../../components/Carousel'
 import OngCarousel from '../../components/CarouselOngs'
 import Footer from '../../components/Footer'
+import banner from '../../utils/images/banner.png'
 
 const Home = () => {
   return (
     <>
       <Navbar />
 
+      <Box display="flex" justifyContent="center">
+        <img src={banner} alt="banner com anuncio de frete gratis" />
+      </Box>
       <Box marginLeft="20%" w="60%">
         <Text fontSize="2xl" color="#17214D">
           Novidades
