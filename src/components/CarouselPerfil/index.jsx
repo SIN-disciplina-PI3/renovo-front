6ca45808a703500649333f2370d0968c0ad9c966
Igor Candido <igor.00000847958@unicap.br>
2023-11-19 17:53:11 -0300

    feat: Mudança tag Grid

--- src/components/CarouselPerfil/index.jsx
+++ src/components/CarouselPerfil/index.jsx
@@ -11,7 +11,9 @@ import {
   TabPanels,
   Tab,
   TabPanel,
-  SimpleGrid
+  SimpleGrid,
+  Grid,
+  GridItem
 } from '@chakra-ui/react'
 
 function PefilCarousel() {
@@ -19,26 +21,26 @@ function PefilCarousel() {
 
   return (
     <>
-        <Tabs marginTop={20}>
-          <TabList>
-            <Tab>Anúncios</Tab>
-            <Tab>Doações</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <SimpleGrid columns={6}>
-                {produtos.map((produto, index) => (
-                  <Box key={index} marginTop={5} marginLeft={5}>
-                    <SectionProduct produtoProp={produto} />
-                  </Box>
-                ))}
-              </SimpleGrid>
-            </TabPanel>
-            <TabPanel>
-              <p>a definir</p>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
+      <Tabs isLazy marginTop={20}>
+        <TabList>
+          <Tab>Anúncios</Tab>
+          <Tab>Doações</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Grid templateColumns="repeat(6, 1fr)" gap={4}>
+              {produtos.map((produto, index) => (
+                <GridItem key={index}>
+                  <SectionProduct  produtoProp={produto} />
+                </GridItem>
+              ))}
+            </Grid>
+          </TabPanel>
+          <TabPanel>
+            <p>a definir</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </>
   )
 }
